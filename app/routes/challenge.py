55b3e47806da48b3483e2f9acbc5fb8ef2444964
LucasWaisten <waistenlucas@gmail.com>
2023-06-03 15:12:36 -0300

    desarrollo el endpoint POST del challenge

--- app/routes/challenge.py
+++ app/routes/challenge.py
@@ -1,33 +1,73 @@
-from fastapi import APIRouter
+from bson import ObjectId
+from fastapi import APIRouter, Depends, Request
+from app.models.challenge import ChallengeCreate, ChallengeResponse
+from app.auth.auth_utils import decode_token, get_token_from_header
 
-router = APIRouter()
+router_challenge = APIRouter()
 
-@router.post("/athletes/me/challenges")
-async def create_challenge():
-    # Lógica para crear un nuevo desafío
-    pass
 
-@router.patch("/athletes/me/challenges/{id_challenge}")
+@router_challenge.post("/", response_model=ChallengeResponse)
+async def create_challenge(request: Request,
+                           challenge: ChallengeCreate,
+                           authorization: str = Depends(get_token_from_header)):
+    challenges = request.app.database["challenges"]
+    # Obtener el ID del usuario del token
+    user_id = decode_token(authorization)["id"]
+
+    # Crear un nuevo desafío en la base de datos
+    new_challenge = {
+        "user_id": user_id,
+        "title": challenge.title,
+        "description": challenge.description,
+        "metric": challenge.metric,
+        "limit_time": challenge.limit_time or None,
+        "state": "NO_INICIADA",
+        "list_multimedia": [],
+        "list_goals": []
+    }
+    challenge_id = await challenges.insert_one(new_challenge)
+
+    # Construir la respuesta del desafío creado
+    response = ChallengeResponse(
+        id=str(challenge_id.inserted_id),
+        user_id=user_id,
+        title=challenge.title,
+        description=challenge.description,
+        metric=challenge.metric,
+        limit_time=challenge.limit_time,
+        state="NO_INICIADA",
+        list_multimedia=[],
+        list_goals=[]
+    )
+
+    return response
+
+
+@router_challenge.patch("/{id_challenge}")
 async def update_challenge(id_challenge: str):
     # Lógica para actualizar un desafío existente
     pass
 
-@router.delete("/athletes/me/challenges/{id_challenge}")
+
+@router_challenge.delete("/{id_challenge}")
 async def delete_challenge(id_challenge: str):
     # Lógica para eliminar un desafío existente
     pass
 
-@router.get("/athletes/me/challenges/{id_challenge}")
+
+@router_challenge.get("/{id_challenge}")
 async def get_challenge(id_challenge: str):
     # Lógica para obtener un desafío específico
     pass
 
-@router.get("/athletes/me/challenges")
+
+@router_challenge.get("/")
 async def get_challenges():
     # Lógica para obtener todos los desafíos del atleta
     pass
 
-@router.patch("/athletes/me/challenges/{id_challenge}/goals")
+
+@router_challenge.patch("/{id_challenge}/goals")
 async def add_goal_to_challenge(id_challenge: str):
     # Lógica para agregar una meta a un desafío
     pass
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
+from app.routes.challenge import router_challenge
+
 api_router = APIRouter()
 @api_router.get("/", tags=["Home"])
 def get_root() -> dict:
     return {"message": "OK"}
+
+api_router.include_router(router_challenge, tags=["Athletes Challenges"], prefix="/athletes/me/challenges")
